9606ed2aef41660bb5f844b37690a52736cbf5a3
bivdub <bivdub@gmail.com>
2014-12-11 07:31:55 -0800

    made logon system work in class

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-
+var bcrypt = require('bcrypt');
 var db = require('./models');
+var session = require('express-session');
 
 app.set('view engine','ejs');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(session({
+    secret:'s14y3rR00lz',
+    resave: false,
+    saveUnitialized: true
+}))
+
+app.use(function(req,res, next) {
+    req.getUser = function() {
+        return req.session.user || false;
+    }
+    next();
+})
 
 app.get('/',function(req,res){
-    res.render('index',{user:false});
+    var user = req.getUser();
+    res.render('index',{user:user});
 });
 
 app.get('/restricted',function(req,res){
-    res.render('restricted');
+    if(req.getUser()){
+        res.render('restricted');
+    } else {
+        res.send('ACCESS DENIED')
+    }
 });
 
 //login form
@@ -24,9 +42,26 @@ app.get('/auth/login',function(req,res){
 
 app.post('/auth/login',function(req,res){
     //do login here (check password and set session value)
-
+    db.user.find({where:{email:req.body.email}}).then(function(userObj) {
+        if(userObj){
+            bcrypt.compare(req.body.password, userObj.password, function(err, match) {
+                if (match === true) {
+                    req.session.user = {
+                        id: userObj.id,
+                        email: userObj.email,
+                        name: userObj.name
+                    }
+                    res.redirect('/');
+                } else {
+                    res.send('not woot!');
+                }
+            })
+        }else{
+            res.send('unkown jerkwad');
+        }
+    });
     //user is logged in forward them to the home page
-    res.redirect('/');
+    // res.redirect('/');
 });
 
 //sign up form
@@ -36,15 +71,29 @@ app.get('/auth/signup',function(req,res){
 
 app.post('/auth/signup',function(req,res){
     //do sign up here (add user to database)
-
+    db.user.findOrCreate(
+        {
+            where: {email: req.body.email}, 
+            defaults: {email: req.body.email, name: req.body.name, password: req.body.password}
+        }).spread(function(user, created) {
+        if (created == true) {
+            res.send({thing: 'User Created'});
+        } else {
+            res.send({thing:'User Exists'});
+        }
+          
+    }).catch(function(error) {
+        res.send(error);
+    })
     //user is signed up forward them to the home page
-    res.redirect('/');
+    // res.redirect('/');
 });
 
 //logout
 //sign up form
 app.get('/auth/logout',function(req,res){
-    res.send('logged out');
+    delete req.session.user;
+    res.redirect('/');
 });
 
 app.listen(3000);
--- models/user.js
+++ models/user.js
@@ -1,17 +1,39 @@
 "use strict";
 
+var bcrypt = require('bcrypt');
+
+
 module.exports = function(sequelize, DataTypes) {
   var user = sequelize.define("user", {
     email: DataTypes.STRING,
-    password: DataTypes.STRING,
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [5,1000],
+          msg: "Please Use a password at least 5 letters"
+        }
+      }
+    },
     name: DataTypes.STRING
-  }, {
+  }, 
+  { 
     classMethods: {
       associate: function(models) {
         // associations can be defined here
       }
+    },
+    hooks: {
+      beforeCreate: function(data, garbage, sendback) {
+        var passwordToEncrypt = data.password;
+        bcrypt.hash(passwordToEncrypt, 10, function (err, hash) {          
+          data.password = hash;
+          sendback(null, data);
+        })
+      }
     }
-  });
+  
+});
 
   return user;
 };
